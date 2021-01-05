12b97974e265775a139427a883ecd3782274c648
omprakash <omprakash201194@gmail.com>
2021-01-05 20:41:25 +0530

    Day 5: Discussion Solution Remove Duplicates from Sorted List II

--- src/com/gautam/january/RemoveDuplicatesFromSortedList.java
+++ src/com/gautam/january/RemoveDuplicatesFromSortedList.java
@@ -3,6 +3,18 @@ package com.gautam.january;
 /**
  * @author gautam
  * Created on 05-Jan-21 at 7:02 PM.
+ *
+ * Given the head of a sorted linked list, delete all nodes that have duplicate numbers,
+ * leaving only distinct numbers from the original list. Return the linked list sorted as well.
+ *
+ * Input: head = [1,2,3,3,4,4,5]
+ * Output: [1,2,5]
+ *
+ *
+ * Example 2:
+ * Input: head = [1,1,1,2,3]
+ * Output: [2,3]
+ *
  */
 public class RemoveDuplicatesFromSortedList {
     public static void main(String[] args) {
@@ -12,52 +24,39 @@ public class RemoveDuplicatesFromSortedList {
         ListNode.print(listNode);
     }
 
+    /**
+     * Delete duplicates list node.
+     *
+     * @param head the head
+     * @return the list node
+     *
+     * Reference: https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/579/week-1-january-1st-january-7th/3593/discuss/1003000/Simple-java-solution-with-explaination-0ms-100-faster
+     * 1.To avoid head null checks create dummy node and point dummy.next=head.
+     * 2.Take two pointers. prev and temp. prev point to dummy initially and temp point to head.
+     * 3. loop till temp and temp next is available. (as for single node nothing to be deleted)
+     * 4. if matching element found keep on incrementing temp without changing previous inside loop.
+     * 5. after loop finishes at point 4 we use boolean to check if some duplicates has found. if yes just point prev.next=temp.next. else just increment prev.
+     * 6. in any case if duplicate exists or not increment temp to point to next element.
+     */
     public static ListNode deleteDuplicates(ListNode head) {
-        if (head == null) {
-            return head;
+        ListNode dummy= new ListNode(101,head);
+        ListNode prev = dummy;
+        ListNode temp = head;
+        boolean match;
+        while(temp!=null && temp.next!=null) {
+            match=false;
+            while(temp.next!=null && temp.next.val==temp.val) {
+                temp=temp.next;
+                match=true;
+            }
+            if(match) {
+                prev.next=temp.next;
+            } else {
+                prev=prev.next;
+            }
+            temp=temp.next;
         }
-        ListNode startNode = null, previousNode = null, currentIterator = head, nextCurrent = head.next;
-        // return the node if the size= 1
-        if (nextCurrent == null) {
-            return head;
-        }
-        while (nextCurrent != null) {
-            if (currentIterator.val !=nextCurrent.val) {
-                if (startNode == null) {
-                    startNode = currentIterator;
-                    previousNode = startNode;
-                } else {
-                    previousNode.next = currentIterator;
-                    previousNode = currentIterator;
-                }
-            }
-            // Keep iterating until you find different values
-            while (nextCurrent != null && currentIterator.val == nextCurrent.val) {
-                nextCurrent = nextCurrent.next;
-            }
-            currentIterator = nextCurrent;
-            if (currentIterator == null) {
-                if (previousNode != null) {
-                    previousNode.next = currentIterator;
-                }
-                return startNode;
-            }
-            nextCurrent = nextCurrent.next;
-
-            //Handle last node
-            if (nextCurrent == null) {
-                if (previousNode != null) {
-                    previousNode.next = currentIterator;
-                }
-                //If first node is not initialized fix it
-                if (startNode == null){
-                    startNode = currentIterator;
-                    previousNode = startNode;
-                }
-            }
-
-        }
-        return startNode;
+        return dummy.next;
     }
 }
 
